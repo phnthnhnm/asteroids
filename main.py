f81afdc4fbf8ea5864ce1ebd571c7b02c311063b
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-05 20:35:11 +0700

    player can move up and down

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
  
-    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, PLAYER_RADIUS)
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
     while True:
         for event in pygame.event.get():
--- player.py
+++ player.py
@@ -3,8 +3,8 @@ from constants import *
 from circleshape import CircleShape
 
 class Player(CircleShape):
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
     def triangle(self):
@@ -21,6 +21,10 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -28,3 +32,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)   
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
